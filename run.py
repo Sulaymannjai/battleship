9a2f93f53d9e2971f29a135ed6277796ff0e92cd
Sulaymannjai <>
2023-10-20 13:46:11 +0000

    setting up the ships

--- run.py
+++ run.py
@@ -4,3 +4,7 @@ import random
 board_size = 10
 board = [['O' for _ in range(board_size)] for _ in range(board_size)]
 
+# Putting the ships
+ship_row = random.randint(0, board_size - 1)
+ship_col = random.randint(0, board_size - 1)
+
