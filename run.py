93e8fdd194e7602bf6e625d61d66dd6649e04bcc
Sulaymannjai <>
2023-10-20 13:55:45 +0000

    started readme.md and edited run.py

--- run.py
+++ run.py
@@ -9,9 +9,10 @@ ship_row = random.randint(0, board_size - 1)
 ship_col = random.randint(0, board_size - 1)
 
 # Receiving your imputs
-for _ in range(4):  # Allow the player 4 turns to guess the battleship's location
+for _ in range(4): 
     print("Turn", _ + 1)
     guess_row = int(input("Guess Row: "))
     guess_col = int(input("Guess Col: "))
 
-    
+
+print("Current Board:")
