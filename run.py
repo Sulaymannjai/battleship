1fce43bdffb6fdcbdb76a1b8cf69d3f651bd0e94
Sulaymannjai <>
2023-10-23 13:32:51 +0000

    changed concept

--- run.py
+++ run.py
@@ -1,40 +1,55 @@
 import random
 
-# Creating the board
-board_size = 5
-board = [['O' for _ in range(board_size)] for _ in range(board_size)]
+# Creating the boad
+BOARD_SIZE = 5
+MAX_TURNS = 4
+EMPTY = 'O'
+HIT = 'X'
+board = [[EMPTY for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
 
-# Putting the ships
-ship_row = random.randint(0, board_size - 1)
-ship_col = random.randint(0, board_size - 1)
+# Placeing the Ships
+ship_row = random.randint(0, BOARD_SIZE - 1)
+ship_col = random.randint(0, BOARD_SIZE - 1)
 
-# Receiving your imputs
-for _ in range(4):
+def print_board(board):
+    """
+    Welcome to My BattleShip
+    """
+    for row in board:
+        print(" ".join(row))
 
-    print("Turn", _ + 1)
-    guess_row = int(input("Guess Row: "))
-    guess_col = int(input("Guess Col: "))
+def is_valid_input(row, col):
+    return 0 <= row < BOARD_SIZE and 0 <= col < BOARD_SIZE
 
-    if guess_row == ship_row and guess_col == ship_col:
-        print("Conratutations!! you sunk my battleship")
-        break
-else:
-    if guess_row not in range(
-        board_size
-    ) or guess_col not in range(board_size):
-        print("Oops, that's not even in the ocean.")
-    elif board[guess_row][guess_col] == 'X':
-        print("You guessed that one already.")
-    else:
-        print("You missed my battleship!")
-        board[guess_row][guess_col] = 'X'
+def play_battleship():
+    print("Welcome to Battleship Game!")
+    print_board(board)
 
-print("Current Board:")
-for row in board:
-    print(" ".join(row))
+    for turn in range(1, MAX_TURNS + 1):
+        print("Turn", turn)
+        guess_row = int(input("Guess Row: "))
+        guess_col = int(input("Guess Col: "))
 
-if _ == 3:
-    print(
-        "Game Over. The battleship was located at row",
-        ship_row, "and column", ship_col
-    )
+        if not is_valid_input(guess_row, guess_col):
+            print("Oops, that's not even in the ocean.")
+        elif board[guess_row][guess_col] == HIT:
+            print("You guessed that one already.")
+        elif guess_row == ship_row and guess_col == ship_col:
+            print("Congratulations! You sunk my battleship!")
+            board[guess_row][guess_col] = HIT
+            break
+        else:
+            print("You missed my battleship!")
+            board[guess_row][guess_col] = HIT
+
+        print("Current Board:")
+        print_board(board)
+
+        if turn == MAX_TURNS:
+            print(
+                "Game Over. The battleship was located at row",
+                ship_row, "and column", ship_col
+            )
+
+if __name__ == "__main__":
+    play_battleship()
